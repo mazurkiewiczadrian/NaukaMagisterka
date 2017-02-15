96d4ca7bc14e1a5f0b7ba641081f13ae56ae1333
mazurkiewiczadrian <mazurkiewicz.adrian.93@gmail.com>
2017-02-15 22:10:31 +0100

    Dodanie klasy GameWorld

--- gen/com/example/naukamagisterka/R.java
+++ gen/com/example/naukamagisterka/R.java
@@ -22,8 +22,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int blueflsh=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int whiteflsh=0x7f020003;
     }
     public static final class id {
         public static final int SCROLLER_ID=0x7f080000;
--- src/com/example/naukamagisterka/GameWorld.java
+++ src/com/example/naukamagisterka/GameWorld.java
@@ -1,10 +1,23 @@
 package com.example.naukamagisterka;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Observer;
 import java.util.TreeMap;
+
+import org.w3c.dom.Node;
+
+import android.widget.Button;
 
 public class GameWorld {
 	int index;
+	Button[] btn;
+	final int row_org_matrix;
+	final int col_org_matrix;
+	final int tog_row_matrix;
+	final int tog_col_matrix;
+	final int n;
+	
 	
 	//Macierz, która posiada wartoœci true(dla zapalonego œwiat³a) i false dla zgaszonego
 	public boolean [][]orig_matrix;
@@ -24,10 +37,193 @@ public class GameWorld {
 	public TreeMap<Integer, boolean[]> tm = new TreeMap<Integer, boolean[]>();
 	
 	public GameWorld(int level) {
-		//Konstruktor do zrobienia
+		index = (level-1)/5;
+		orig_matrix = new boolean[Global.MATRIX_SIZE[index][0]][Global.MATRIX_SIZE[index][1]];
+		row_org_matrix = orig_matrix.length;
+		col_org_matrix = orig_matrix[0].length;
+		
+		toogle_matrix = new boolean[row_org_matrix*col_org_matrix][row_org_matrix*col_org_matrix];
+		tog_row_matrix = toogle_matrix.length;
+		tog_col_matrix = toogle_matrix[0].length;
+		
+		vector_matrix = new boolean[row_org_matrix*col_org_matrix];
+		result_matrix = new boolean [row_org_matrix*col_org_matrix];
+		result_matrix_v1 = new boolean [row_org_matrix*col_org_matrix];
+		result_matrix_v2 = new boolean [row_org_matrix*col_org_matrix];
+		result_matrix_v12 = new boolean [row_org_matrix*col_org_matrix];
+		
+		n = row_org_matrix*col_org_matrix;
+		btn = new Button[n+1];
+		col.put(true, R.drawable.blueflsh);
+		col.put(false, R.drawable.whiteflsh);
+	}
+	
+	/* To nie dzia³a ale istnieje wbudowana funkcja 
+	 * public void notifyObservers()
+	   Checks the internal flag to see if the observable has changed
+       state and notifies all observers.
+	 * 
+	 * 
+	public void notifyAllObservers(){
+		for (Observer observer : observers){
+			observer.update();
+		}
+	}
+	*/
+
+	public void buttonNumber(int c){
+		pressButton((c-1)/col_org_matrix, (c-1)%col_org_matrix);
+		pressButton((c-1)/col_org_matrix-1, (c-1)%col_org_matrix);
+		pressButton((c-1)/col_org_matrix-1, (c-1)%col_org_matrix);
+		pressButton((c-1)/col_org_matrix, (c-1)%col_org_matrix-1);
+		pressButton((c-1)/col_org_matrix, (c-1)%col_org_matrix-1);
 	}
 	
 	public void pressButton(int row, int col){
+		if (row >= 0 && row < row_org_matrix && col >=0 && col < col_org_matrix) {
+			orig_matrix[row][col] = orig_matrix[row][col]^true;
+		}
+	}
+	
+	public void click(int row, int col, int cnt){
+		if (row >= 0 && row < row_org_matrix && col >= 0 && col < col_org_matrix) {
+			toogle_matrix[(row)*col_org_matrix+(col)][cnt] = true;
+		}
+	}
+	
+	public void buildToggleMatrix(){
+		int cnt = 0;
+		for (int i = 1; i <= row_org_matrix * col_org_matrix; i++) {
+			click((i-1)/col_org_matrix, (i-1)%col_org_matrix, cnt);
+			click((i-1)/col_org_matrix+1, (i-1)%col_org_matrix, cnt);
+			click((i-1)/col_org_matrix-1, (i-1)%col_org_matrix, cnt);
+			click((i-1)/col_org_matrix, (i-1)%col_org_matrix+1, cnt);
+			click((i-1)/col_org_matrix, (i-1)%col_org_matrix-1, cnt);
+			cnt++;
+		}
+	}
+	
+	public boolean allButtonOff(){
+		for (int i = 0; i < row_org_matrix; i++) {
+			for (int j = 0; j < col_org_matrix; j++) {
+				if (orig_matrix[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	int findPivot(int startRow, int pivotColumn){
+		for (int row = startRow; row < tog_row_matrix; ++row) {
+			if (toogle_matrix[row][pivotColumn]) {
+				return row;
+			}
+		}return -1;
+	}
+	
+	public void Reduce(){
+		int cnt = 0;
+		for (int i = 0; i < row_org_matrix; i++) {
+			for (int j = 0; j < col_org_matrix; j++) {
+				vector_matrix[cnt]=orig_matrix[i][j];
+				cnt++;
+			}
+		}
+		//Wype³nia ka¿dy rz¹d 1.0
+		for (boolean[] row : toogle_matrix) {
+			Arrays.fill(row, false);
+		}
+		buildToggleMatrix();
 		
+		for (int i = 0; i < n; i++) {
+			result_matrix[i]=false;
+			result_matrix_v1[i]=false;
+			result_matrix_v2[i]=false;
+			result_matrix_v12[i]=false;
+		}
+		int nextFreeRow = 0;
+		
+		for (int col = 0; col < n; ++col) {
+			int pivotRow = findPivot(nextFreeRow, col);
+			if (pivotRow == (-1)) {
+				continue;
+			}
+			for (int i = 0; i < tog_row_matrix; i++) {
+				boolean temp = toogle_matrix[pivotRow][i];
+				toogle_matrix[pivotRow][i] = toogle_matrix[nextFreeRow][i];
+				toogle_matrix[nextFreeRow][i] = temp;
+			}
+			
+			boolean temp2 = vector_matrix[pivotRow];
+			vector_matrix[pivotRow]=vector_matrix[nextFreeRow];
+			vector_matrix[nextFreeRow]=temp2;
+			
+			for (int row = pivotRow+1; row < n; ++row) {
+				if (toogle_matrix[row][col]) {
+					for (int i = 0; i < tog_col_matrix; i++) {
+						toogle_matrix[row][i]=toogle_matrix[row][i]^toogle_matrix[nextFreeRow][i];
+					}
+					vector_matrix[row] ^= vector_matrix[nextFreeRow];
+				}
+			}
+			++nextFreeRow;
+		}
+	}
+	
+	public int substitute(){
+		for (int row = n-1; row >= 0; row--) {
+			int pivot = -1;
+				for (int col = 0; col < n; ++col) {
+					if (toogle_matrix[row][col]) {
+						pivot = col;
+						break;
+					}
+				}
+				
+				if (pivot == (-1)) {
+					if (vector_matrix[row]) {
+						System.out.println("Brak rozwi¹zania");
+						return -1;
+					}
+				}else{
+					result_matrix[row] = vector_matrix[row];
+					for (int col = pivot + 1; col < n; ++col) {
+						result_matrix[row] = 
+						(result_matrix[row]^(toogle_matrix[row][col]&result_matrix[col]));
+					}
+				}
+				
+		}return 0;
+	}
+	
+	public void optimalButtons(){
+		int var1 =0, var2=0, var3=0,var4=0;
+		tm.clear();
+		
+		for (int i = 0; i < n; i++) {
+			result_matrix_v1[i] = result_matrix[i]^Global.NULL_SPACE[index][0][i];
+			result_matrix_v2[i]=result_matrix[i]^Global.NULL_SPACE[index][1][i];
+			result_matrix_v12[i] = result_matrix[i]^Global.NULL_SPACE[index][0][i]^
+					Global.NULL_SPACE[index][1][i];
+			
+				if (result_matrix[i]) {
+					++var1;
+				}
+				if (result_matrix_v1[i]) {
+					++var2;
+				}
+				if (result_matrix_v2[i]) {
+					++var3;
+				}
+				if (result_matrix_v12[i]) {
+					++var4;
+				}
+		}
+		
+		tm.put(var1, result_matrix);
+		tm.put(var2, result_matrix_v1);
+		tm.put(var3, result_matrix_v2);
+		tm.put(var4, result_matrix_v12);
 	}
 }
