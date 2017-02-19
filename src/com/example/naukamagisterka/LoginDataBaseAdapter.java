0f8eeda62d0247487b9e273bd02e08f78941cee6
mazurkiewiczadrian <mazurkiewicz.adrian.93@gmail.com>
2017-02-19 21:08:07 +0100

    Dodanie do tabei USERS pola POINTS.
    Uzupełnienie klasy LoginDataBaseAdapter o metodę pozwalająca na
    wyciągnięcie liczby punktów zalogowanego użytkownika.

--- src/com/example/naukamagisterka/LoginDataBaseAdapter.java
+++ src/com/example/naukamagisterka/LoginDataBaseAdapter.java
@@ -15,10 +15,11 @@ public class LoginDataBaseAdapter {
 	public String Email;
 	public String Password;
 	public int LEVEL;
+	public int POINTS;
 	
 	LoginDataBaseAdapter loginDataBaseAdapter;
 	
-	static final String DATABASE_CREATE = "CREATE TABLE IF NOT EXISTS "+"USERS"+"(USERNAME VARCHAR UNIQUE, PASSWORD VARCHAR, LEVEL INT)"+";"+"";
+	static final String DATABASE_CREATE = "CREATE TABLE IF NOT EXISTS "+"USERS"+"(USERNAME VARCHAR UNIQUE, PASSWORD VARCHAR, LEVEL INT, POINTS INT)"+";"+"";
 	
 	public SQLiteDatabase db;
 	private final Context context;
@@ -49,7 +50,8 @@ public class LoginDataBaseAdapter {
 		
 		newValues.put("USERNAME", userName);
 		newValues.put("PASSWORD", password);
-		newValues.put("LEVEL", 0);
+		newValues.put("LEVEL", 1);
+		newValues.put("POINTS", 0);
 		
 		db.insert("USERS", null, newValues);
 	}
@@ -111,11 +113,29 @@ public class LoginDataBaseAdapter {
 		
 	}
 	
-	public void updateEntry(String userName, String password, String level){
+	public String getSingleEntryPoints(String userName){
+		Cursor cursor = db.query("USERS", null, " USERNAME=?", new String[]{userName},null,null,null);
+		if (cursor.getCount()<1) //Nie ma username
+		{
+			cursor.close();
+			return "NOT EXIST";
+		}
+		
+		else{
+		cursor.moveToFirst();
+		String points = cursor.getString(cursor.getColumnIndex("POINTS"));
+		cursor.close();
+		return points;
+		}
+		
+	}
+	
+	public void updateEntry(String userName, String password, String level, String points){
 			ContentValues updateValues = new ContentValues();
 			updateValues.put("USERNAME", userName);
 			updateValues.put("PASSWORD", password);
 			updateValues.put("LEVEL", level);
+			updateValues.put("POINTS", points);
 			
 			String where = "USERNAME=?";
 			db.update("USERS", updateValues, where, new String[]{userName});
