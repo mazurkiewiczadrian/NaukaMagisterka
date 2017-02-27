659ac239cadb51ec8a9003cade4fb3dd613ec6d4
mazurkiewiczadrian <mazurkiewicz.adrian.93@gmail.com>
2017-02-27 11:52:56 +0100

    Dodanie testowych textview do wyświetlenia Punktów i Levelu

--- gen/com/example/naukamagisterka/R.java
+++ gen/com/example/naukamagisterka/R.java
@@ -29,28 +29,30 @@ public final class R {
     }
     public static final class id {
         public static final int SCROLLER_ID=0x7f080000;
-        public static final int action_settings=0x7f080016;
-        public static final int backTextView=0x7f080015;
-        public static final int buttonSignUp=0x7f080014;
-        public static final int button_exit=0x7f08000b;
-        public static final int button_instructions=0x7f080009;
-        public static final int button_logout=0x7f08000a;
-        public static final int button_start=0x7f080008;
-        public static final int confirmPasswordSignUp=0x7f080013;
-        public static final int email=0x7f08000c;
-        public static final int emailSignUp=0x7f080011;
-        public static final int email_login_form=0x7f080007;
-        public static final int email_sign_in_button=0x7f08000e;
-        public static final int imageView1=0x7f080003;
-        public static final int ll1=0x7f080002;
-        public static final int ll2=0x7f080004;
-        public static final int ll3=0x7f08000f;
-        public static final int login_form=0x7f080006;
-        public static final int login_progress=0x7f080005;
-        public static final int password=0x7f08000d;
-        public static final int passwordSignUp=0x7f080012;
-        public static final int signUpTextView=0x7f080010;
+        public static final int action_settings=0x7f080018;
+        public static final int backTextView=0x7f080017;
+        public static final int buttonSignUp=0x7f080016;
+        public static final int button_exit=0x7f08000d;
+        public static final int button_instructions=0x7f08000b;
+        public static final int button_logout=0x7f08000c;
+        public static final int button_start=0x7f08000a;
+        public static final int confirmPasswordSignUp=0x7f080015;
+        public static final int email=0x7f08000e;
+        public static final int emailSignUp=0x7f080013;
+        public static final int email_login_form=0x7f080009;
+        public static final int email_sign_in_button=0x7f080010;
+        public static final int imageView1=0x7f080005;
+        public static final int ll1=0x7f080004;
+        public static final int ll2=0x7f080006;
+        public static final int ll3=0x7f080011;
+        public static final int login_form=0x7f080008;
+        public static final int login_progress=0x7f080007;
+        public static final int password=0x7f08000f;
+        public static final int passwordSignUp=0x7f080014;
+        public static final int signUpTextView=0x7f080012;
         public static final int textView1=0x7f080001;
+        public static final int textViewLevel=0x7f080003;
+        public static final int textViewPoints=0x7f080002;
     }
     public static final class layout {
         public static final int about=0x7f030000;
